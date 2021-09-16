fe4a463dd76dd8641f24d4fbe88df271d610c863
Crowdbotics <team@crowdbotics.com>
2021-09-16 19:23:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "round_rice_30621Identifier",
+  url: "https://round_rice_30621Identifier.botics.co",
+  api: "https://round_rice_30621Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
